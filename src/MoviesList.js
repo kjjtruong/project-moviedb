48b8014d9f07507b7a35f3aa22cb6361e6e3c0bf
kentruong <kjjtruong@gmail.com>
2019-05-09 11:00:04 -0600

    first commit

---
+++ src/MoviesList.js
@@ -0,0 +1,45 @@
+/* eslint-disable no-console */
+/* eslint-disable react/destructuring-assignment */
+import React, { PureComponent } from 'react';
+import styled from 'styled-components';
+import Movie from './Movie';
+
+class MoviesList extends PureComponent {
+  state = {
+    movies: [],
+  };
+
+  async componentDidMount() {
+    try {
+      const res = await fetch(
+        'https://api.themoviedb.org/3/discover/movie?api_key=92c67836ff0bf14b489475f1954afa48&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1',
+      );
+      const movies = await res.json();
+      console.log(movies);
+      this.setState({
+        movies: movies.results,
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  render() {
+    return (
+      <MovieGrid>
+        {this.state.movies.map(movie => (
+          <Movie key={movie.id} movie={movie} />
+        ))}
+      </MovieGrid>
+    );
+  }
+}
+
+export default MoviesList;
+
+const MovieGrid = styled.div`
+  display: grid;
+  padding: 1rem;
+  grid-template-columns: repeat(6, 1fr);
+  grid-row-gap: 1rem;
+`;
